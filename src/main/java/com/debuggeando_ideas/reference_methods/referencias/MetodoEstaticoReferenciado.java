328ab6095fba52a48d1528659495685faacbf850
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-06 03:30:39 -0600

    Metodo referencia de forma estatica

--- src/main/java/com/debuggeando_ideas/reference_methods/referencias/MetodoEstaticoReferenciado.java
+++ src/main/java/com/debuggeando_ideas/reference_methods/referencias/MetodoEstaticoReferenciado.java
@@ -1,11 +1,21 @@
 package com.debuggeando_ideas.reference_methods.referencias;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 public class MetodoEstaticoReferenciado {
 
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString());
+        // System.out.println(UUID.randomUUID().toString());
+
+        Supplier<UUID> getToken = () -> UUID.randomUUID();
+        System.out.println("getToken: " + getToken.get());
+
+
+        // Metodo referenciado de forma estatica
+        Supplier<UUID> getTokenStaticMethodReference = UUID::randomUUID;
+        System.out.println("getToken reference: " + getTokenStaticMethodReference.get());
+
     }
 
 }
