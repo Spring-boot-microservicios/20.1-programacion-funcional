7544987c786bf1f096f184edb3ce9cbc2cacb810
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-12 22:14:05 -0600

    Operadores de findFirst y findAny

--- src/main/java/com/debuggeando_ideas/streams/stream_final_operators/StreamFinalOperators.java
+++ src/main/java/com/debuggeando_ideas/streams/stream_final_operators/StreamFinalOperators.java
@@ -3,6 +3,7 @@ package com.debuggeando_ideas.streams.stream_final_operators;
 import com.debuggeando_ideas.util.Database;
 import com.debuggeando_ideas.util.Videogame;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamFinalOperators {
@@ -35,6 +36,16 @@ public class StreamFinalOperators {
         System.out.println("noneMatchOperator isEmpty: " + result);
     }
 
+    private static void findFirstOperator(Stream<Videogame> stream) {
+        Optional<Videogame> videogame = stream.findFirst();
+        System.out.println("findFirstOperator: " + videogame);
+    }
+
+    private static void findAnyOperator(Stream<Videogame> stream) {
+        Optional<Videogame> videogame = stream.findAny();
+        System.out.println("findAnyOperator: " + videogame);
+    }
+
     public static void main(String[] args) {
         Stream<Videogame> videogames = Database.videogames.stream();
 
@@ -45,6 +56,10 @@ public class StreamFinalOperators {
         // anyMatchOperator(videogames); // Regresa true porque el predicado o condicion cumple con el parametro
         // allMatchOperator(videogames); // Regresa false porque no todos han vendido mas de 150
         // noneMatchOperator(videogames); // false porque al menos uno no cumple el resultado
+
+        // findFirstOperator(videogames); // Obtiene el primer elemento y requiere de un Optional
+        // findAnyOperator(videogames); // Regresa cualquier elemento pero casi siempre es el primero
+
     }
 
 }
