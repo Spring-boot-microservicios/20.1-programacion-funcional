fdb4146b3ec1761a9a77bd099efd64b28def4fa8
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-12 22:08:59 -0600

    operadores de anyMatch, allMatch y noneMatch

--- src/main/java/com/debuggeando_ideas/streams/stream_final_operators/StreamFinalOperators.java
+++ src/main/java/com/debuggeando_ideas/streams/stream_final_operators/StreamFinalOperators.java
@@ -15,12 +15,36 @@ public class StreamFinalOperators {
         stream.forEach(System.out::println);
     }
 
+    private static void anyMatchOperator(Stream<Videogame> stream) {
+        boolean result = stream.anyMatch(videogame -> videogame.getTotalSold() > 1);
+
+        // Vendidos
+        System.out.println("anyMatchOperator > 1: " + result);
+    }
+
+    private static void allMatchOperator(Stream<Videogame> stream) {
+        boolean result = stream.allMatch(videogame -> videogame.getTotalSold() > 150);
+
+        // Vendidos
+        System.out.println("allMatchOperator > 150: " + result);
+    }
+
+    private static void noneMatchOperator(Stream<Videogame> stream) {
+        // Sino tiene alguna reseña
+        boolean result = stream.noneMatch(videogame -> videogame.getReviews().isEmpty());
+        System.out.println("noneMatchOperator isEmpty: " + result);
+    }
+
     public static void main(String[] args) {
         Stream<Videogame> videogames = Database.videogames.stream();
 
         // Operadores finales, no permite continuar, falla por ese esta comentado
         // countOperator(videogames);
-        forEachOperator(videogames);
+        // forEachOperator(videogames);
+
+        // anyMatchOperator(videogames); // Regresa true porque el predicado o condicion cumple con el parametro
+        // allMatchOperator(videogames); // Regresa false porque no todos han vendido mas de 150
+        // noneMatchOperator(videogames); // false porque al menos uno no cumple el resultado
     }
 
 }
