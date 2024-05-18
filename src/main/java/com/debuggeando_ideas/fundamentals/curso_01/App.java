e4cede77487c8bb3568cc4528a6d91c14f7ccbc7
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-18 05:50:18 -0600

    Generar un Debugger en Intellij community vista de stream

--- src/main/java/com/debuggeando_ideas/fundamentals/curso_01/App.java
+++ src/main/java/com/debuggeando_ideas/fundamentals/curso_01/App.java
@@ -1,8 +1,37 @@
 package com.debuggeando_ideas.fundamentals.curso_01;
 
+import com.debuggeando_ideas.util.Database;
+import com.debuggeando_ideas.util.Review;
+import com.debuggeando_ideas.util.Videogame;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class App {
 
     public static void main(String[] args) {
+
+        Stream<Videogame> videogames = Database.videogames.stream();
+
+        /**
+         * DEBUG en streams -> seleccionamos var myArray = videogames (breakpoint)
+         * se ejecuta en modo debug
+         * aparecen las opciones del debugger y seleccionamos en more o los tres puntos
+         * seleccionamos el Trace Current Stream Chain para ver la vista
+         */
+
+        var myArray = videogames
+            .distinct()
+            .sorted(Comparator.comparing(Videogame::getTotalSold))
+            .flatMap(videogame ->
+                    videogame.getReviews().stream().map(
+                        Review::getComment
+                    )
+            ).filter(comment -> comment.contains("best"))
+            .map("Good comment: "::concat)
+            .toArray();
+
+        System.out.println("myArray: " + myArray);
 
     }
 
