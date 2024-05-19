f4fa5abc3e1b8da6aada3a4893450c2a11b7cca8
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-18 18:57:32 -0600

    Agrupacion por tipo consola con collectors

---
+++ src/main/java/com/debuggeando_ideas/collectors/collector_collection/CollectorsGroupBy.java
@@ -0,0 +1,24 @@
+package com.debuggeando_ideas.collectors.collector_collection;
+
+import com.debuggeando_ideas.util.Console;
+import com.debuggeando_ideas.util.Database;
+import com.debuggeando_ideas.util.Videogame;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectorsGroupBy {
+
+    public static void main(String[] args) {
+        Stream<Videogame> videogames = Database.videogames.stream();
+
+        // Estamos haciendo particiones de grupos por consola
+        Map<Console, List<Videogame>> result = videogames.collect(Collectors.groupingBy(Videogame::getConsole));
+
+        result.forEach((k, v) -> System.out.println(k + " - " + v));
+
+    }
+
+}
