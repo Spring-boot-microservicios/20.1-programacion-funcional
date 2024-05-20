60c952335563864775fca609d26dc896362079c0
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-19 19:08:26 -0600

    Ejemplo de complejidad ciclomatica

---
+++ src/main/java/com/debuggeando_ideas/complejidad_ciclomatica/InvalidVideogameException.java
@@ -0,0 +1,9 @@
+package com.debuggeando_ideas.complejidad_ciclomatica;
+
+public class InvalidVideogameException extends RuntimeException {
+
+    public InvalidVideogameException(String message) {
+        super(message);
+    }
+
+}
